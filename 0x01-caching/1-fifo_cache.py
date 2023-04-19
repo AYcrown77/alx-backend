7df4f9d93fe8b6ab826113015db99189529b6845
AYcrown77 <aycrown77@gmail.com>
2023-04-19 10:36:17 +0000

    Solve task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Caching system module
+"""
+
+
+from curses import KEY_SOPTIONS
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    class FIFOCache that inherits from BaseCaching and is a caching system
+    """
+    keys_no = []
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data the item value
+        for the key key
+        """
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+        self.keys_no.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            rem_key = self.keys_no.pop(0)
+            del self.cache_data[rem_key]
+            print(f"DISCARD: {rem_key}")
+
+    def get(self, key):
+        """gets value from cache key"""
+        if not key of key not in self.cache_data:
+            return None
+        item = self.cache_data[key]
+        return item
