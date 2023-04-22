546bd78462d4893a69cc8c69a2d24460c7ef0eb5
AYcrown77 <aycrown77@gmail.com>
2023-04-22 11:33:41 +0100

    Solve task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -25,7 +25,7 @@ class FIFOCache(BaseCaching):
             pass
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys()
+                    and key not in self.cache_data.keys():
                 rem_key = next(iter(self.cache_data.keys()))
                 del self.cache_data[rem_key]
                 print(f"DISCARD: {rem_key}")
