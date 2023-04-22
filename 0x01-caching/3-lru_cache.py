eefb3a3a39de3ea7337aae4399b5d1369cbd924e
AYcrown77 <aycrown77@gmail.com>
2023-04-22 12:10:23 +0100

    Solve task 2

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,33 +8,32 @@ class LRUCache(BaseCaching):
     """
     class LRUCache that inherits from BaseCaching and is a caching system
     """
-    count_dict = {}
 
     def __init__(self):
         """Initialization"""
         super().__init__()
+        self.use_keys = []
 
     def put(self, key, item):
     """
     assign to the dictionary self.cache_data the item value for the key key
     """
-    if not key or not item:
-        return
-    if key not in self.cache_data.keys():
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-            rem_key = list(self.count_dict.keys())[0]
-            print(f"DISCARD: [rem_key]")
+    if key is not None and item is not None:
+        self.cache_data[key] = item
+        if key is not in self.use_keys:
+            self.use_keys.append(key)
+        else:
+            self.use_keys.append(
+                self.use_keys.pop(self.use_keys.index(key)))
+
+        if len(self.use_keys) >  BaseCaching.MAX_ITEMS:
+            rem_key = self.use_keys.pop(0)
             del self.cache_data[rem_key]
-            del self.count_dict[rem_key]
-    if key in self.count_dict.keys():
-        del self.count_dict[key]
-    self.count_dict[key] = 1
-    self.cache_data[key] = item
+            print(f"DISCARD: [rem_key]")
 
     def get(self, key):
         """gets value from cache key"""
-        if not key or not in self.cache_data.key():
-            return None
-        del self.count_dict[key]
-        self.count_dict[key] = 1
-        return self.cache_data[key]
+        if key is not None and key in  self.cache_data.key():
+            self.use_keys.append(self.use_keys.pop(self.use_keys.index(key)))
+            return self.cache_data[key]
+        return None
