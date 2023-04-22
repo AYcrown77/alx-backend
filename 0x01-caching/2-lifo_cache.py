3a4f8d8a0050e9cd3736bb6d5a9d66bd8494210f
AYcrown77 <aycrown77@gmail.com>
2023-04-22 11:45:39 +0100

    Solve task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,7 +11,6 @@ class LIFOCache(BaseCaching):
     """
     class FIFOCache that inherits from BaseCaching and is a caching system
     """
-    keys_no = []
 
     def __init__(self):
         """Initialization"""
@@ -22,22 +21,19 @@ class LIFOCache(BaseCaching):
         Must assign to the dictionary self.cache_data the item value
         for the key key
         """
-        if not key or not item:
-            return
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            self.keys_no.append(key)
-            return
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-            rem_key = self.keys_no.pop()
-            del self.cache_data[rem_key]
-            print(f"DISCARD: {rem_key}")
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                rem_key, rem_value = self.cache_data.popitem()
+                del self.cache_data[rem_key]
+                print(f"DISCARD: {rem_key}")
         self.cache_data[key] = item
-        self.keys_no.append(key)
 
     def get(self, key):
         """gets value from cache key"""
-        if not key of key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         new = self.cache_data[key]
         return new
