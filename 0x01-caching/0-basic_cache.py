8d992b844ac1140d812a46420cacfd608693b286
AYcrown77 <aycrown77@gmail.com>
2023-04-22 11:02:11 +0100

    Solve task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,6 +11,7 @@ class BasicCache(BaseCaching):
     """
     class BasicCache that inherits from BaseCaching and is a caching system
     """
+
     def __init__(self):
         """initialization"""
         super().__init__()
@@ -18,8 +19,9 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache"""
         if not key or not item:
-            return
-        self.cache_data[key] = item
+            pass
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
