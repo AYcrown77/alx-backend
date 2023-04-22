0f8621e1bf770b086624e5096b7a6fbdb033ada2
AYcrown77 <aycrown77@gmail.com>
2023-04-22 11:05:44 +0100

    Solve task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,13 +18,13 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if not key or not item:
+        if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
-        if key is none or key not in self.cache_data.keys():
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
